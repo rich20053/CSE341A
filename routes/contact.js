462b845e234e2b857ff9c2f658b3518739594cef
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2024-01-20 20:11:41 -0700

    Working Version

---
+++ routes/contact.js
@@ -0,0 +1,13 @@
+
+/*** Contact ***/
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contact');
+
+router.get('/', contactsController.getAll);
+
+router.get('/:id', contactsController.getSingle);
+
+module.exports = router;
+
